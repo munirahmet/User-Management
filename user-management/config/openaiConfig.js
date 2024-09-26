bbde2f5ab49514369f17df99db6ed931ffc7b477
Münir Ahmet Özer <127790891+munirahmet@users.noreply.github.com>
2024-09-26 10:39:57 +0300

    Delete user-management directory

--- user-management/config/openaiConfig.js
+++
@@ -1,9 +0,0 @@
-const { Configuration, OpenAIApi } = require('openai');
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-module.exports = openai;
